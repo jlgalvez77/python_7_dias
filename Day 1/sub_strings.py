b1bca893d57bcc004453d915e1d9df03778f2554
Jose Galvez <jlgalvez77@gmail.com>
2025-03-27 00:45:47 +0100

    27/03/25-Medrugada

--- Day 1/sub_strings.py
+++ Day 1/sub_strings.py
@@ -1,8 +1,12 @@
+# Substrings in Python
+
 message = 'Hello World'
 print(message)
 
-sub_string_hello = message[0 : 5]
+# Substring Hello
+sub_string_hello = message[:5]
 print(sub_string_hello)
 
+# Substring World
 sub_string_world = message[6:]
 print(sub_string_world)
--- Day 1/variables.py
+++ Day 1/variables.py
@@ -6,18 +6,19 @@ print(my_int)
 my_int = 20
 print(my_int)
 
+# Print variables
 print(my_float)
 print(my_string)
 
 # Rules to name variables
-# 1.Start with leter or _
+# 1.Start with letter or _
 _my_variable = 30
 
-# 2.Can continur whith letters, numbers or _
+# 2.Can continue with letters, numbers or _
 # 3my_variable = 40 # Error
 _my_variable123456 = 40
 
-# 3. Upper and lower sensity
+# 3. Upper and lower sensitive
 # print(My_variable2) # Error
 print(_my_variable)
 
@@ -25,5 +26,9 @@ print(_my_variable)
 # class = 50 #Error
 klass = 50
 print(klass)
+
+# good practices in Python
+# snake case notation
+user_name = 'Jose'
 
 
