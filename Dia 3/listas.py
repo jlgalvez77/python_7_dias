39de38f5d3673f5fb2c0b25ebcd320a701db53ee
Jose Galvez <jlgalvez@gmail.com>
2025-04-20 15:34:25 +0200

    20/04/25-15:35

---
+++ Dia 3/listas.py
@@ -0,0 +1,14 @@
+print('*** Listas en Python ***')
+nombres = ['Karla', 'Juan', 'Laura']
+# print(f'Lista de nombres: {nombres}')
+
+# Lista heterogenea (multiples tipos de datos)
+lista_heterogenea = [100, True, 'Ivonne']
+# print(f'Lista heterogenea: {lista_heterogenea}')
+
+# Iterar una lista
+# for nombre in nombres:
+#    print(nombre, end=' ')
+
+# Lista de números
+numeros = [100, 200, 300, 400, 500]
